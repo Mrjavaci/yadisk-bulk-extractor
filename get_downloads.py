b6fcee379a822683398e22b28f8cd2122d82b166
Efe TOKER <efetoker@gmail.com>
2021-01-23 23:16:18 +0300

    up

--- get_downloads.py
+++ get_downloads.py
@@ -44,7 +44,7 @@ class getDownloadsSpider(scrapy.Spider):
         cursor.execute("SELECT * FROM `scrapy`.`yadisk` WHERE `safe` = 1 AND `ext` = 'pdf' AND `filename` LIKE '% - %' AND `filename` ORDER BY filename ASC, length(filename) DESC, filesize DESC LIMIT 3500 OFFSET 0")
         rows = cursor.fetchall()
         for book in rows:
-            yield scrapy.Request(url=book[7], callback=self.parse_url, meta={'handle_httpstatus_all': True, 'name': book[3], 'hash': book[8]})
+            yield scrapy.Request(url=book[7], callback=self.parse_url, meta={'handle_httpstatus_all': True, 'id': book[0], 'name': book[3], 'hash': book[8]})
     
     def parse_url(self, response):
         json_obj = json.loads(response.css("script#store-prefetch ::text").extract_first())
@@ -54,7 +54,7 @@ class getDownloadsSpider(scrapy.Spider):
         yield scrapy.Request(
             method='POST', 
             callback=self.finalize,
-            meta={'handle_httpstatus_all': True, "hash":hash, 'name': response.meta['name']}, 
+            meta={'handle_httpstatus_all': True, "hash":hash, 'name': response.meta['name'], 'id': response.meta['id']}, 
             url='https://yadi.sk/public/api/download-url', 
             body=str(urllib.parse.quote(json.dumps({"hash":hash, "sk":sk, "options":{"hasExperimentVideoWithoutPreview":True}}))), 
             headers={"Content-Type": "text/plain", "Cookies": self.get_cookie(response)}
@@ -67,19 +67,25 @@ class getDownloadsSpider(scrapy.Spider):
             yield scrapy.Request(
                 method='POST',
                 callback=self.finalize,
-                meta={'handle_httpstatus_all': True, "hash":response.meta['hash'], 'name': response.meta['name']}, 
+                meta={'handle_httpstatus_all': True, "hash":response.meta['hash'], 'name': response.meta['name'], 'id': response.meta['id']}, 
                 url='https://yadi.sk/public/api/download-url', 
                 body=str(urllib.parse.quote(json.dumps({"hash":response.meta['hash'], "sk":res_body['newSk'], "options":{"hasExperimentVideoWithoutPreview":True}}))), 
                 headers={"Content-Type": "text/plain", "Cookies": self.get_cookie(response)}
             )
         else:
-            folder = self.mega.find(str(response.meta['name'][0]).lower())
-            if folder is None:
-                self.mega.create_folder(str(response.meta['name'][0]).lower())
+            try:
                 folder = self.mega.find(str(response.meta['name'][0]).lower())
-            
-            file_url = res_body['data']['url']
-            testfile = urllib.request.urlretrieve(file_url, response.meta['name'])
-            file = self.mega.upload(response.meta['name'], folder[0])
-            os.remove(response.meta['name'])
-            print(response.meta['name'] + " uploaded")
+                if folder is None:
+                    self.mega.create_folder(str(response.meta['name'][0]).lower())
+                    
+                folder = self.mega.find(str(response.meta['name'][0]).lower())
+                file_url = res_body['data']['url']
+                testfile = urllib.request.urlretrieve(file_url, response.meta['name'])
+                file = self.mega.upload(response.meta['name'], folder[0])
+                os.remove(response.meta['name'])
+                cursor = self.conn.cursor()
+                cursor.execute("UPDATE yadisk SET downloaded = 1 WHERE id = " + response.meta['id'])
+                self.conn.commit()
+                print(response.meta['name'] + " uploaded")
+            except:
+                pass
