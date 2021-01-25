4bb3f4303947b3300c3b226c778488c00262b2ce
Efe TOKER <e.toker@medyatakip.com>
2021-01-25 16:58:36 +0300

    useful pdf scripts added

---
+++ rename_files.py
@@ -0,0 +1,19 @@
+from os import listdir
+import os
+
+mypath = "Z:\E-Books"
+
+for dir in listdir(mypath):
+    for file in listdir(mypath + "\\" + dir):
+        file_path = mypath + "\\" + dir + "\\" + file
+        name_arr = file.split(' - ')
+        if len(name_arr) > 2:
+            if name_arr[0] == name_arr[1]:
+                name_arr.pop(0)
+                new_file_path = mypath + "\\" + dir + "\\" + " - ".join(name_arr)
+                if os.path.exists(new_file_path):
+                    os.remove(file_path)
+                    print("Silindi: " + file_path)
+                else:
+                    os.rename(file_path, new_file_path)
+                    print("Değişti: " + new_file_path)
