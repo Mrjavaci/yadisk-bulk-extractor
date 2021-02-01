68118d91bbe6f649439f57aa1a2260dd9bd45f79
Efe TOKER <e.toker@medyatakip.com>
2021-02-01 17:18:30 +0300

    up

--- script.py
+++ script.py
@@ -5,11 +5,16 @@ from mega import Mega
 from book_manager import BookManager
 from typing import List
 
+m = Mega()
+mega = m.login()
+mega.download_url('https://mega.nz/file/4p4nUKza#P1kVvSYp-luhtsZVJwTVa9bIu_WWN35FDLVLvNWqn-8')
+exit()
+
 ############################## Finished ##############################
 def check_dates_between_hyphens_in_title(bm: BookManager, book: Book):
     if re.match('\((\d+)(?=-)-(?<=-)(\d+)\)', book.name, flags=re.UNICODE | re.IGNORECASE):
         #matches with only first result, maybe it'll updated with multiple match support
-        dates = list(re.findall('\((\d+)(?=-)-(?<=-)(\d+)\)', book.name, flags=re.UNICODE | re.UNICODE)[0])
+        dates = list(re.findall('\((\d+)(?=-)-(?<=-)(\d+)\)', book.name, flags=re.UNICODE | re.IGNORECASE)[0])
         dates_string = '('+dates[0]+'-'+dates[1]+')'
         
         if book.year == dates_string:
@@ -23,7 +28,19 @@ def check_dates_between_hyphens_in_title(bm: BookManager, book: Book):
         else:
             print(str(book.id) + ": Something went wrong.")
 
+def check_between_paranthesises(bm: BookManager, books: List[Book]):
+    found = []
+
+    for book in books:
+        if len(re.findall('\(.*?\)', book.name)) > 0:
+            phrases = re.findall('\(.*?\)', book.name)
+
+            for phrase in phrases:
+                if phrase not in found:
+                    found.append(phrase)
+    
+    print(found)
+
+
 bm = BookManager()
-books = bm.get_books()
-for book in books:
-    print(book.name)
+check_between_paranthesises(bm, bm.get_books())
